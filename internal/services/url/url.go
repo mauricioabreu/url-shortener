bf005f7d30311773e73330a36216111140d3708d
Maurício Antunes <mauricio.abreua@gmail.com>
2024-11-06 20:30:24 -0300

    Remove tmp

--- internal/services/url/url.go
+++ internal/services/url/url.go
@@ -11,12 +11,20 @@ import (
 
 const shortURLLength = 8
 
-func Shorten(ctx context.Context, db *pgxpool.Pool, longURL string) (string, error) {
+type URLService struct {
+	db *pgxpool.Pool
+}
+
+func NewURLService(db *pgxpool.Pool) *URLService {
+	return &URLService{db: db}
+}
+
+func (s *URLService) Shorten(ctx context.Context, longURL string) (string, error) {
 	hash := sha256.Sum256([]byte(longURL))
 	shortURL := base64.URLEncoding.EncodeToString(hash[:])
 	shortURL = strings.TrimRight(shortURL, "=")[:shortURLLength]
 
-	_, err := db.Exec(ctx,
+	_, err := s.db.Exec(ctx,
 		"INSERT INTO urls (long_url, short_url) VALUES ($1, $2)",
 		longURL, shortURL)
 	if err != nil {
