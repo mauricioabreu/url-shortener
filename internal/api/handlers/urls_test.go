4dbdfc38826e7b10621ac590d9f14bad0e899277
Maurício Antunes <mauricio.abreua@gmail.com>
2024-11-10 09:04:32 -0300

    Test requests with tags

--- internal/api/handlers/urls_test.go
+++ internal/api/handlers/urls_test.go
@@ -31,30 +31,55 @@ func TestShorten(t *testing.T) {
 
 	t.Run("valid request", func(t *testing.T) {
 		w := httptest.NewRecorder()
-
 		resp := &url.ShortenResponse{
 			ShortURL: "https://shortener.com/abc123xy",
 		}
 		mockService.EXPECT().Shorten(gomock.Any(), gomock.Any()).Return(resp, nil)
-
 		input := handlers.ShortenRequest{URL: "https://www.google.com"}
 		jsonData, _ := json.Marshal(input)
+
 		req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(jsonData))
 		req.Header.Set("Content-Type", "application/json")
-
 		router.ServeHTTP(w, req)
 
 		assert.Equal(t, http.StatusOK, w.Code)
 	})
 
-	t.Run("invalid url", func(t *testing.T) {
+	t.Run("valid request with tags", func(t *testing.T) {
 		w := httptest.NewRecorder()
-
-		input := handlers.ShortenRequest{URL: "not-a-valid-url"}
+		resp := &url.ShortenResponse{
+			ShortURL: "https://shortener.com/abc123xy",
+		}
+		mockService.EXPECT().Shorten(gomock.Any(), gomock.Any()).Return(resp, nil)
+		input := handlers.ShortenRequest{URL: "https://www.google.com", Tags: []string{"tag1", "tag2"}}
 		jsonData, _ := json.Marshal(input)
+
 		req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(jsonData))
 		req.Header.Set("Content-Type", "application/json")
+		router.ServeHTTP(w, req)
 
+		assert.Equal(t, http.StatusOK, w.Code)
+	})
+
+	t.Run("invalid tags", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		input := handlers.ShortenRequest{URL: "https://www.google.com", Tags: []string{"invalid-tag!"}}
+		jsonData, _ := json.Marshal(input)
+
+		req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(jsonData))
+		req.Header.Set("Content-Type", "application/json")
+		router.ServeHTTP(w, req)
+
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+	})
+
+	t.Run("invalid url", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		input := handlers.ShortenRequest{URL: "not-a-valid-url"}
+		jsonData, _ := json.Marshal(input)
+
+		req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(jsonData))
+		req.Header.Set("Content-Type", "application/json")
 		router.ServeHTTP(w, req)
 
 		assert.Equal(t, http.StatusBadRequest, w.Code)
@@ -62,14 +87,12 @@ func TestShorten(t *testing.T) {
 
 	t.Run("database error", func(t *testing.T) {
 		w := httptest.NewRecorder()
-
 		mockService.EXPECT().Shorten(gomock.Any(), gomock.Any()).Return(nil, assert.AnError)
-
 		input := handlers.ShortenRequest{URL: "https://www.google.com"}
 		jsonData, _ := json.Marshal(input)
+
 		req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(jsonData))
 		req.Header.Set("Content-Type", "application/json")
-
 		router.ServeHTTP(w, req)
 
 		assert.Equal(t, http.StatusInternalServerError, w.Code)
